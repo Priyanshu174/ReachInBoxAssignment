262933434a6a4d4d8275ee41324f6c18d302b033
deepverto11 <deepbhaman11.1234@gmail.com>
2024-08-27 00:34:56 +0530

    first commit

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,15 @@
+function AppBar() {
+  return (
+    <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+      <div className="">
+        <img
+          src="https://s3-alpha-sig.figma.com/img/37f3/2626/1680d9613629542a2474f79f92f7c022?Expires=1725840000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ak8ed-A0oC2xQPtxNjBlss6riMVn70av2vFMjQ~rEdTiaCd70Z80aI6ye7fn-9nQ87E9A8ba0KzcSKRj-8gascEYAIfikQ1idnVCrrDih0B0xmA5Do61CgslWc30M3iL8oXQi6GiT29TGRRmqT4SRrUbVVmCRLkypQ4FGqQggbNjCmP-oO8vPiMXwgZTo0vr3Je6y~JHUj-phU2LArRBmvrz4kZxhzHzKR1zVDYv10MV31p3WuJML7Jt6WRNdU-PrGEn-1XxDRm-ksA1~IXuxnupxt4pO1Q4OEAclP~dGAgtx3sDj9Y539juQ6utY0rRlaZhKlSri1YXJ12yMKqa~A__"
+          alt="logo"
+          className=" h-10"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
