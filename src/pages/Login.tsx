b359b64902d2334d11034073240394baabe1fc69
deepverto11 <deepbhaman11.1234@gmail.com>
2024-08-27 01:10:58 +0530

    updated localhost url to vercel deployed url

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,7 @@ function Login() {
   }
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://priyanshu-reach-in-box-ai-assignment.vercel.app/";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-in-box-assignment-ruby.vercel.app/";
   };
   
   return (
